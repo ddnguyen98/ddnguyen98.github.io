25a3018c745e18a12640030017fca447bec2d85a
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2019-02-15 05:41:01 -0500

    Finished JSON, will talk about it in class at some point

--- BikeShop/js/content.js
+++ BikeShop/js/content.js
@@ -11,16 +11,16 @@ xhr.onload = function() {
     let responseObj = JSON.parse(xhr.responseText);
 
     if(document.URL.includes("index.html")){
-        IndexJSON(responseObj)
+        IndexJSON(responseObj);
     }
     else if(document.URL.includes("shop.html")){
-
+        ShopJSON(responseObj);
     }
     else if(document.URL.includes("club_events.html")){
-
+        ClubEventsJSON(responseObj);
     }
     else if(document.URL.includes("blog.html")){
-
+        BlogJSON(responseObj);
     }
 };
 
@@ -92,16 +92,82 @@ function IndexJSON(response){
 function ShopJSON(response){
     let insertContent = '';
     let products = response.products;
+
+    for (let i = 0; i < products.length; ++i) {
+        if(products[i].colors.length === 1){
+            insertContent = '<img src="'+products[i].imageURL+'" alt="'+products[i].title+' that is in the color ${responseObj.products[i].color[0]}" width="100" height="100">';
+        }
+        else{
+            let colorString = '';
+            //Creates color string
+            for (let e = 0; e < products[i].colors.length; ++e ){
+                if(e === 0){
+                    colorString+= products[i].colors[e];
+                }
+                else if(e !== products[i].colors.length - 1){
+                    colorString+= ', '+products[i].colors[e];
+                }
+                else{
+                    colorString+= ' or '+products[i].colors[e];
+                }
+            }
+            //Other img if colors contains more than 1
+            insertContent = '<img src="'+products[i].imageURL+'" alt="'+products[i].title+' that is available in the colors '+colorString+'" width="100" height="100">';
+        }
+        if(products[i].salePrice.length === 0){
+            insertContent += '<p>$'+products[i].price+'</p>';
+        }
+        else{
+            insertContent += '<p>$'+products[i].salePrice+' $<del>'+products[i].price+'</del></p>';
+        }
+        insertContent += '<h4>'+products[i].title+'</h4>';
+        insertContent +=
+            '<button>Add To Cart</button>' +
+            '<button>Like</button>' +
+            '<button>Zoom in</button>';
+
+        let content = document.querySelector('section#shop article:nth-of-type('+(i+1)+')');
+        content.innerHTML = insertContent;
+    }
 }
 function ClubEventsJSON(response){
     let insertContent = '';
     let events = response.events;
+    //Looks through event json
+    for(let i = 0; i < events.length; ++i){
+        insertContent = '<p>'+events[i].date+'</p>';
+        insertContent += '<h3>'+events[i].title+'</h3>';
+        insertContent += '<p>Located at '+events[i].location+' '+events[i].text+'</p>';
+        insertContent += '<a href="">RSVP here</a>';
 
+        //updates current html
+        let content = document.querySelector('section#events article:nth-of-type('+(i+1)+')');
+        content.innerHTML = insertContent;
+    }
 }
 function BlogJSON(response){
     let insertContent = '';
     let posts = response.posts;
 
+    //Recent posts (First two on JSON file)
+    for (let i = 0; i < 2; ++i){
+        insertContent = '<h3><a href="">'+posts[i].title+'</a></h3>';
+
+        let content = document.querySelector('section#rp ul>li:nth-of-type('+(i+1)+')');
+        content.innerHTML = insertContent;
+    }
+
+    //Posts that populate page
+    for (let i = 0; i < posts.length; ++i){
+        insertContent = '<h3>'+posts[i].title+'</h3>';
+        //Placeholder for images
+        insertContent += '<p>'+posts[i].postDate+'</p>';
+        insertContent += '<p>'+posts[i].text+'</p>';
+        insertContent += '<a href="">Read More</a>';
+
+        let content = document.querySelector('section#posts article:nth-of-type('+(i+1)+')');
+        content.innerHTML = insertContent;
+    }
 }
 
 //Gets data from jsonFile
